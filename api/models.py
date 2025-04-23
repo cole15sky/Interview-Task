9fa043bce109afdb64d3e0046497a3ad480889dc
Sky Blue <dawadiaakash@gmail.com>
2025-04-23 22:53:01 +0545

    feat: initialize Task model to obtain CRUD operations

--- api/models.py
+++ api/models.py
@@ -38,3 +38,22 @@ class CustomUser(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+    
+    
+    
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    )
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
